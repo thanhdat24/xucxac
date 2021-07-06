93bd1c5934a4ddb8f78104bcf4e4ef262fd92865
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-07-06 21:38:20 +0700

    Update redux

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+import GameXucXacReducer from "./GameXucXacReducer";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  GameXucXacReducer,
+});
+
+export default rootReducer;
